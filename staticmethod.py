f94c2263748c83f8f0e2aa436a17e05c964de3b6
caiofariaas <caiofarias077@gmail.com>
2023-11-19 23:27:51 -0300

    staticmethod

---
+++ staticmethod.py
@@ -0,0 +1,50 @@
+# Estudo staticmethod
+# staticmethod's não possuem acesso aos atributos de uma classe, ele não pode receber 'self' ou 'cls' como parametro!
+# também não são acessados a partir de instancias de classe, são acessados apenas pela classe em sí.
+
+class Calculadora:
+
+# Criação de dois staticmethod's
+
+    @staticmethod
+    def somar(a, b):
+        return a + b
+    
+    @staticmethod
+    def subtrair(a, b):
+        return a - b
+    
+# Aqui um exemplo de seu uso
+    
+result = Calculadora.somar(10, 20)
+
+print(result)
+
+# Neste segundo exemplo vamos criar uma classe 'Usuario'
+
+class Usuario:
+    def __init__(self, nome: str, idade: int, senha: str):
+        
+# Inicializando os atributos da classe
+
+        self.nome = nome
+        self.idade = idade
+        self.senha = senha
+        
+# Aqui vamos criar um staticmethod que verifica se a senha digitada por um usuário é válida!
+
+    @staticmethod
+    def validar(senha):
+        if len(senha) >= 8:
+            return "\nSenha criada com sucesso!\n"
+        else:
+            return "\nSenha inválida! crie uma senha com no mínimo 8 caracteres.\n"
+        
+# Pedimos a entrada do usuário, neste caso a Senha
+        
+senha = input("Digite uma senha de no mínimo 8 caracteres: ")
+
+# E aqui chamados o método a partir da classe e passando como parametro a variável responsável por armazenar a senha!
+
+print(Usuario.validar(senha))
+        
